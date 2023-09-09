24cc686a9ab1bc538f3082658aac718d3a80ce95
kashyapAmansingh <coder85113@gmail.com>
2023-09-09 10:53:00 +0530

    code- full working

--- Component/TopicList.jsx
+++ Component/TopicList.jsx
@@ -1,34 +1,61 @@
-import React from 'react'
-import Removebtn from './Removebtn'
+"use client"
+
+
+
+import React, { useEffect, useState } from 'react';
+import Removebtn from './Removebtn';
 import { FcEditImage } from 'react-icons/fc';
 import Link from 'next/link';
 
-const TopicList = () => {
-    return (<>
-        <div className='container-fluid  '>
-            <div className='row'>
-                <div className='d-flex '>
+const getTopics = async () => {
+  try {
+    const apiPort = process.env.NEXT_PUBLIC_API_PORT || 3000; // Default to 3000 if not set
+    const apiUrl = `http://localhost:${apiPort}/api/topics`;
 
-                    <h2   className='fw-bold '>Topic Title</h2>
-                    <Removebtn />
-                    <div >            
-                    <Link href={'/editTopic/123'}><FcEditImage /> Edit</Link> </div>
-                    <div>
-
-                    </div>
-
-                </div>
-
-            </div>
-            <div className='row'>
-                <div className='d-flex  '>
-                    <h2  className='fw-bold ml-5'>Topic Description</h2>
-                
-                </div>
-            </div>
-        </div>
-    </>
-    )
+    const res = await fetch(apiUrl, {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics", error);
+  }
 }
 
-export default TopicList
+export default function TopicList() {
+  const [topics, setTopics] = useState([]);
+
+  useEffect(async () => {
+    const { topics } = await getTopics();
+    setTopics(topics);
+  }, []);
+
+  return (
+    <>
+      {topics.map((value) => (
+        <div className='container-fluid mt-3 mb-4' key={value._id}>
+          <div className='row'>
+            <div className='d-flex'>
+              <h2 className='fw-bold '>Topic Title: {value.title}</h2>
+              {/* <h2 className='fw-bold '>Topic createdAt: {value.createdAt}</h2> */}
+
+              <Removebtn />
+              <div>
+                <Link href={`/editTopic/${value._id}`}>
+                  <FcEditImage /> Edit
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='d-flex'>
+              <h2 className='fw-bold ml-5'>Topic Description: {value.description}</h2>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
--- src/app/addTopic/page.jsx
+++ src/app/addTopic/page.jsx
@@ -1,17 +1,53 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
 
-const addTopic= () => {
+const AddTopic = () => {
+  const [title, setTitles] = useState('');
+  const [description, setdescription] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+
+    if (!title || !description) {
+      alert("Please enter a title and description.");
+      return;
+    }
+
+    const apiPort = process.env.NEXT_PUBLIC_API_PORT || 3000;
+    const apiUrl = `http://localhost:${apiPort}/api/topics`;
+
+    try {
+      const res = await fetch(apiUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title, description })
+      });
+
+      if (res.ok) {
+        console.log(res.ok);
+        return (
+          window.location.href = '/'
+        );
+      } else {
+        throw new Error("Sorry, we failed to create Topics");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-        <form>
-        <input type="text" placeholder='Topic Title' />
-        <br/>
-            <input type="text" placeholder='Topic Description' />
-      <br/>
-      <button  className='btn btn-info mt-1'>Add topics</button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Topic Title' value={title} onChange={(e) => setTitles(e.target.value)} />
+        <br />
+        <input type="text" placeholder='Topic Description' value={description} onChange={(e) => setdescription(e.target.value)} />
+        <br />
+        <button type="submit" className='btn btn-info mt-1' >Add topics</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default addTopic
+export default AddTopic;
