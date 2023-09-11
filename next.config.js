4c8ee344ac40350e4c4a5875e1502abc145ccacf
kashyapAmansingh <coder85113@gmail.com>
2023-09-11 22:50:02 +0530

    Bug- Corss verce

--- next.config.js
+++ next.config.js
@@ -2,19 +2,13 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      loader: 'cloudinary',
-      path: 'https://res.cloudinary.com/dm2wuzfzc/images/upload',
-    },
-     async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3000'
-      }
-    ]
-  }
+   
+ 
   };
   
   module.exports = nextConfig;
   
+  // images: {
+  //   loader: 'cloudinary',
+  //   path: 'https://res.cloudinary.com/dm2wuzfzc/images/upload',
+  // },
