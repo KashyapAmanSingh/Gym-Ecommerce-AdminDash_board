5d771c50c156d6de683174ea260bc978dcc06085
kashyapAmansingh <coder85113@gmail.com>
2023-09-11 22:34:37 +0530

    Bug- Corss

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
       loader: 'cloudinary',
       path: 'https://res.cloudinary.com/dm2wuzfzc/images/upload',
     },
+     async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3000'
+      }
+    ]
+  }
   };
   
   module.exports = nextConfig;
