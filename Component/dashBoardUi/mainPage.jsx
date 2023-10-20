e7d10559283d38a4e98137958bbc53bf32846824
kashyapAmansingh <coder85113@gmail.com>
2023-10-20 15:36:17 +0530

    Feature- card for the total product,category

--- Component/dashBoardUi/mainPage.jsx
+++ Component/dashBoardUi/mainPage.jsx
@@ -4,25 +4,25 @@ const MainPage = () => {
   return (
     <div>
       <div className="container mt-5">
-
+ 
         <div className="row my-5">
           <div className='d-flex justify-content-between text-center'>
-            <div className="col-sm-4  r bg-white">
-              <h2 className="card-title mt-2 text-muted">Total Orders of Day </h2>
+            <div className="col-sm-4  bg-white">
+              <h2 className="card-title mt-2 text-muted">Today&apos;s orders </h2>
               <h6 className='my-2 text-info fw-bold fs-2'> 250</h6>
-              <p>250 orders today   </p>
+              <p>250 orders since last day  </p>
 
             </div>
             <div className="col-sm-4  bg-white mx-3">
 
-              <h2 className="card-title mt-2 text-muted ">Total Orders of Week </h2>
+              <h2 className="card-title mt-2 text-muted ">  Weekly orders</h2>
               <h6 className='my-2 text-info fw-bold fs-2'> 500</h6>
-              <p>500 orders this week  </p>
+              <p>500 orders since last  week  </p>
             </div>
             <div className="col-sm-4 bg-white">
-              <h2 className="card-title mt-2 text-muted">Total Orders of Month </h2>
+              <h2 className="card-title mt-2 text-muted">  Monthly orders </h2>
               <h6 className='my-2 text-info fw-bold fs-2'> 2000</h6>
-              <p className=''>2000 orders this month   </p>
+              <p className=''>2000 orders since last month   </p>
 
             </div>
           </div>
@@ -31,25 +31,45 @@ const MainPage = () => {
         {/* Total Revenue */}
 
         <div className="row d-flex ">
-        <div className='d-flex justify-content-between text-center '>
+          <div className='d-flex justify-content-between text-center '>
             <div className="col-sm-4  bg-white align-middle">
-              <h2 className=" card-title mt-2 text-muted">Total Revenue Day  </h2>
+              <h2 className=" card-title mt-2 text-muted">Today&apos;s Revenue</h2>
               <h6 className='my-2 text-info fw-bold fs-2' >$5000</h6>
-              <p>$5000 revenue today   </p>
+              <p>$5000 revenue since last day   </p>
 
             </div>
             <div className="col-sm-4  bg-white mx-3 ">
 
-              <h2 className="card-title  mt-2 text-muted">Total Revenue Week  </h2>
+              <h2 className="card-title  mt-2 text-muted">Weekly Revenue   </h2>
               <h6 className='my-2 text-info fw-bold fs-2'>$25000</h6>
-              <p>$25000 revenue this  week </p>
+              <p>$25000 revenue since last  week </p>
             </div>
             <div className="col-sm-4  bg-white align-middle">
 
-              <h2 className="card-title  mt-2 text-muted">Total Revenue Month </h2>
+              <h2 className="card-title  mt-2 text-muted">   Monthly Revenue </h2>
 
-              <h6  className='my-2 text-info fw-bold fs-2'> $100000</h6>
-              <p> $100000 revenue this month   </p>
+              <h6 className='my-2 text-info fw-bold fs-2'> $100000</h6>
+              <p> $100000 revenue since last month  </p>
+
+            </div>
+          </div>
+        </div>
+
+        <div className="row my-5">
+          <div className='d-flex justify-content-between text-center'>
+            <div className="col-sm-4   bg-white">
+              <h2 className="card-title mt-2 text-muted">Total Category </h2>
+              <h6 className='my-2 text-info fw-bold fs-2'>5</h6>
+
+            </div>
+            <div className="col-sm-4 mx-3  bg-white ">
+              <h2 className="card-title mt-2 text-muted">Total Products </h2>
+              <h6 className='my-2 text-info fw-bold fs-2'>18</h6>
+
+            </div>
+            <div className="col-sm-4   bg-white">
+              <h2 className="card-title mt-2 text-muted">Total Customer</h2>
+              <h6 className='my-2 text-info fw-bold fs-2'>05</h6>
 
             </div>
           </div>
